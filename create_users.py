f2bb9426bd5bf599e3c609ddf154179af749304b
LADN Jayawardena <ladnjay@proton.me>
2025-06-03 14:30:57 +0530

    Support Engineer Coding Challenge

---
+++ create_users.py
@@ -0,0 +1,12 @@
+import csv
+import requests
+
+def create_users(file_path):
+        with open(file_path, 'r') as f:
+                 reader = csv.DictReader(f)
+                 for row in reader:
+                       response = requests.post("https://example.com/api/create_user", json=row)
+                       if response.status_code != 201:
+                              print("Error creating user:", row["email"])
+
+create_users("users.csv")
